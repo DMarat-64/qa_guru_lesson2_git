d28f686781efaf388076a19883f0309e533d1d9a
Marat D <Marat@mail.ru>
2025-04-08 23:56:29 +0400

    hello message was changed

--- src/main/java/guru/marat/Main.java
+++ src/main/java/guru/marat/Main.java
@@ -3,18 +3,12 @@ package guru.marat;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static String main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello 1234!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.printf("Salut 1234!");
 
         private static String getMessage() {
-        return "Hello Mir!";
+        return "Salut Mir!";
     }
 }
